502b732f4eeadf734a0290c1563a51bfb721b7ff
Mitchelle <shanellmusarurwa6@gmail.com>
2025-06-13 21:25:48 +0200

    updated app.jsx

--- react task one/src/App.jsx
+++ react task one/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <h1>My Profile</h1>
       <ProfileCard
-        name="Mitchelle"
+        name="Mitchelle Musarurwa"
         image={`${import.meta.env.BASE_URL}mitchelle.jpg`}
         bio="I'm a creative and driven web developer with a passion for building responsive, user-friendly websites and applications. I specialize in front-end development using HTML, CSS, JavaScript, and Bootstrap, and I’m experienced in backend development with Node.js, Express, and MySQL."
       />
