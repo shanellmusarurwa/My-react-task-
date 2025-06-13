9584b58b81a4591f380b6369526500d7039db6a7
Mitchelle <shanellmusarurwa6@gmail.com>
2025-06-13 21:22:26 +0200

    added all components

---
+++ react task one/src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import './App.css';
+import ProfileCard from './components/ProfileCard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Profile</h1>
+      <ProfileCard
+        name="Mitchelle"
+        image={`${import.meta.env.BASE_URL}mitchelle.jpg`}
+        bio="I'm a creative and driven web developer with a passion for building responsive, user-friendly websites and applications. I specialize in front-end development using HTML, CSS, JavaScript, and Bootstrap, and I’m experienced in backend development with Node.js, Express, and MySQL."
+      />
+    </div>
+  );
+}
+
+export default App;
